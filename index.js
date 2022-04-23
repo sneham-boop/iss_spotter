8dc28920585b95b0647643ea94b2c766e0194722
Sneha Mahajan <sneh.km@gmail.com>
2022-04-23 15:11:29 -0400

    Add logic for fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
-fetchMyIP((error, idAddress) => {
+fetchMyIP((error, ipAddress) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  console.log("IP address is: ", idAddress);
+  console.log("IP address is: ", ipAddress);
 });
+
+fetchCoordsByIP('142.112.253.232', (error, coordinates) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log("Coordinates are: ", coordinates);
+});
+
+
--- iss.js
+++ iss.js
@@ -19,6 +19,27 @@ const fetchMyIP = (callback) => {
   });
 };
 
+const fetchCoordsByIP = (ip, callback) => {
+  const path = `https://freegeoip.app/json/${ip}`;
+  request(path, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP, response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    body = JSON.parse(body);
+    const { latitude, longitude } = body;
+    callback(null, { latitude, longitude });
+  });
+};
+
 module.exports = {
   fetchMyIP,
+  fetchCoordsByIP,
 };
