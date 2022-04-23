4e543c40a0aaf949cc2ba18b764c84753f61cd17
Sneha Mahajan <sneh.km@gmail.com>
2022-04-23 15:28:43 -0400

    Add fetchISSFlyOverTimes logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 fetchMyIP((error, ipAddress) => {
   if (error) {
@@ -9,7 +9,7 @@ fetchMyIP((error, ipAddress) => {
   console.log("IP address is: ", ipAddress);
 });
 
-fetchCoordsByIP('142.112.253.232', (error, coordinates) => {
+fetchCoordsByIP("142.112.253.232", (error, coordinates) => {
   if (error) {
     console.log(error);
     return;
@@ -18,4 +18,14 @@ fetchCoordsByIP('142.112.253.232', (error, coordinates) => {
   console.log("Coordinates are: ", coordinates);
 });
 
+fetchISSFlyOverTimes(
+  { latitude: 43.5142, longitude: -79.8845 },
+  (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
 
+    console.log("It worked! :) Returned times: ", data);
+  }
+);
--- iss.js
+++ iss.js
@@ -39,7 +39,29 @@ const fetchCoordsByIP = (ip, callback) => {
   });
 };
 
+const fetchISSFlyOverTimes = ({ latitude, longitude }, callback) => {
+  const path = `https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  request(path, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    console.log(path);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode}, when fetching pass times, response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    body = JSON.parse(body);
+    callback(null, body);
+  });
+};
+
 module.exports = {
   fetchMyIP,
   fetchCoordsByIP,
+  fetchISSFlyOverTimes,
 };
